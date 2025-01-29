87da32a6ff0f58b9978ae9bb012e543aebb366d0
Kman77 <emmanuel.chalot@coda-student.school>
2025-01-29 13:51:18 +0100

    feat: implement habit addition and deletion in HabitController

--- src/controllers/habitController.js
+++ src/controllers/habitController.js
@@ -14,4 +14,33 @@ export default class HabitController {
       form.addEventListener('submit', this.handleAdd.bind(this));
       this.view.list.addEventListener('click', this.handleClick.bind(this));
     }
-}
+  
+    handleAdd(event) {
+      event.preventDefault();
+      const form = event.target;
+      const habit = {
+        
+        name: form.name.value,
+        frequency: form.frequency.value
+      };
+      
+      this.model.addHabit(habit);
+      form.reset();
+      this.view.renderHabits(this.model.habits);
+      this.view.renderChart(this.model.habits);
+    }
+  
+    handleClick(event) {
+      if (event.target.matches('.delete')) {
+        const id = parseInt(event.target.dataset.id);
+        this.model.deleteHabit(id);
+        this.view.renderHabits(this.model.habits);
+        this.view.renderChart(this.model.habits);
+      } else if (event.target.matches('.complete')) {
+        const id = parseInt(event.target.dataset.id);
+        this.model.updateProgress(id, new Date(), true);
+        this.view.renderHabits(this.model.habits);
+        this.view.renderChart(this.model.habits);
+      }
+    }
+  }

f537981b3c9c884163e8040a669420bcb7effc5e
Kman77 <emmanuel.chalot@coda-student.school>
2025-01-29 12:36:00 +0100

    feat: add HabitView and integrate with HabitController in index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './styles.css';
+import HabitView from './views/habitView';
+import HabitController from './controllers/habitController';
+
+const app = new HabitController(new HabitModel(), new HabitView());
